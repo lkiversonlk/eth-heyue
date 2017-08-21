2845c281e25a2e05f51de7356b72c56666c7eba1
liukan <liukan@bytedance.com>
2017-08-21 19:27:51 +0800

    server.js
    Change-Id: I38a0383eb8798a100f18bc806cab0bc2d29fde31

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
-const Index = ()=>(
-	<div>
- 	<p>Hello Next.js</p>
-	</div>
-)
+import Link from 'next/link'
+
+const Index = () => (
+    <div>
+        <Link href="/about" style={{ fontSize: 20 }} >
+            <button>Go to About Page</button>
+        </Link>
+        <p>Hello Next.js</p>
+    </div>
+);
 
 export default Index
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({dev});
+const handle = app.getRequestHandler();
+
+app.prepare()
+.then(() => {
+    const server = express();
+
+    server.listen(4000, (err)=>{
+       if(err) throw err;
+
+       console.log(">Ready on http://localhost:4000");
+    });
+})
