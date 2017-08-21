94f331081a6266504f047c7a96f0c95211176873
liukan <liukan@bytedance.com>
2017-08-21 19:58:20 +0800

    server.js
    Change-Id: I0b3d0f4b2fd81fd22a933e7264dde39ad60b754f

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#ps aux | grep "node server.js" | tr -s " " | cut -d" " -f2 | xargs kill
+cat PID | xargs kill
+git pull
+npm install || true
+npm run build
+NODE_ENV=production nohup node server.js 2>&1 &
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const next = require("next");
-
+const fs = require("fs");
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({dev});
 const handle = app.getRequestHandler();
 
 app.prepare()
-.then(() => {
-    const server = express();
+    .then(() => {
+        const server = express();
 
-    server.listen(4000, (err)=>{
-       if(err) throw err;
-
-       console.log(">Ready on http://localhost:4000");
-    });
-})
+        server.listen(4000, (err) => {
+            if (err) throw err;
+            fs.writeFile("PID", process.pid.toString() + "\n");
+            console.log(">Ready on http://localhost:4000");
+        });
+    })
