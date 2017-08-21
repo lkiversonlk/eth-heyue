70b1c7e8405899b2f2d3133d1261c247a7a33c53
liukan <liukan@bytedance.com>
2017-08-22 00:44:16 +0800

    add json contract interface
    Change-Id: Id5e7a397d57545fbc1bcf4e473f81d7ac6220926

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const fs = require("fs");
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({dev});
 const handle = app.getRequestHandler();
+const Web3 = require("web3");
+const YellowPage = require("eth-yellowpage").EthYellowPage;
+const aync = require("async");
+
+var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+var yellowPage = new YellowPage(web3);
+
+current_contracts = {};
+
+var count = yellowPage.TotalCount();
+console.log("started to load " + count + " contracts");
+yellowPage.StartCache(1);
+
 
 app.prepare()
     .then(() => {
@@ -14,4 +27,17 @@ app.prepare()
             fs.writeFile("PID", process.pid.toString() + "\n");
             console.log(">Ready on http://localhost:4000");
         });
-    })
+
+        /**
+         * allow cros on API
+         */
+        server.use("/api", function (req, res, next) {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+            next();
+        });
+
+        server.get("/api/contracts", function (req, res) {
+            res.json(yellowPage.GetCache() || {});
+        });
+    });
